910296ed05747d9f8ca77223cb821e66d80ca324
Savushkin Zhenya <savik94mail@gmail.com>
2016-06-16 21:10:45 +0300

    added filters

--- src/dashboard/catalog/controller.js
+++ src/dashboard/catalog/controller.js
@@ -4,5 +4,88 @@ export default class CatalogCtrl {
     constructor(staff) {
         this.staff = staff;
         this.filters = [];
+        this.initFilters();
+    }
+
+    handleFilter(filterName) {
+        const filer = this.getFilterByName(filterName) || {};
+        if(filer.exclude) {
+            if(filer.exclude == "all") {
+                this.filters = [];
+            } else {
+                this.removeFilterByName(filer.exclude);
+            }
+        }
+        if(this.isFilterSwitchedOn(filterName)) {
+            this.removeFilterByName(filterName);
+        } else {
+            this.addFilter(filterName);
+        }
+    }
+
+    removeFilterByName(filterName) {
+        const filterIndex = this.filters.findIndex(f => f.name == filterName);
+        if (filterIndex > -1) {
+            this.filters.splice(filterIndex, 1);
+        }
+    }
+
+    addFilter(filterName) {
+        this.filters.push(this.getFilterByName(filterName));
+    }
+
+    isFilterSwitchedOn(filterName) {
+        return this.filters.findIndex(f => f.name == filterName) > -1;
+    }
+
+    getFilterByName(filterName) {
+        return this.allFilters.find(f => f.name == filterName);
+    }
+
+    initFilters() {
+        this.allFilters = [
+            {
+                name: "models",
+                exclude: "all",
+                tagName: "Модели"
+            },
+            {
+                name: "models.male",
+                tagName: "Парни",
+                exclude: "models.female"
+            },
+            {
+                name: "models.female",
+                tagName: "Девушки",
+                exclude: "models.male"
+            },
+            {
+                name: "models.hair.red",
+                tagName: "Рыжие"
+            },
+            {
+                name: "models.hair.dark",
+                tagName: "Темные"
+            },
+            {
+                name: "models.hair.blond",
+                tagName: "Светлые"
+            },
+            {
+                name: "photographs",
+                tagName: "Фотографы",
+                exclude: "all"
+            },
+            {
+                name: "stylists",
+                tagName: "Стилисты",
+                exclude: "all"
+            },
+            {
+                name: "set",
+                tagName: "СПЕЦПРЕДЛОЖЕНИЯ",
+                exclude: "all"
+            }
+        ]
     }
 }
