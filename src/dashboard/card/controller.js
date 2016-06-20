cfe2affbbd0cb864f73704f19d735e352af4980a
Savushkin Zhenya <savik94mail@gmail.com>
2016-06-20 21:39:14 +0300

    added card logic

--- src/dashboard/card/controller.js
+++ src/dashboard/card/controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
 export default class CardCtrl {
-    constructor() {
-    	
+    constructor(cardStorage) {
+    	this.staff = cardStorage.staff;
+    	this.cardStorage = cardStorage;
+    }
+
+    remove(person) {
+        this.cardStorage.removeItem(person);
     }
 }
